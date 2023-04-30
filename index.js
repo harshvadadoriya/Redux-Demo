5f4e640dfabfd51163bfbb610776ae2b60025ac9
Harsh Vadadoriya <harshvadadoriya079@gmail.com>
2023-04-30 11:12:11 +0530

    single reducer but not good option to use

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //? Action
 // define action, action is an object that has "type" property
 const BUY_CAKE = 'BUY_CAKE';
+const BUY_ICECREAM = 'BUY_ICECREAM';
 
 // action creator which create action, i.e. it's a function that returns an action
 function buyCake() {
@@ -10,11 +11,18 @@ function buyCake() {
 	};
 }
 
+function buyIcecream() {
+	return {
+		type: BUY_ICECREAM,
+	};
+}
+
 //? Reducer
 // (previousState,action) => newState
 
 const initialState = {
 	numOfCakes: 10,
+	numOfIceCream: 15,
 };
 
 //! Way-1 Not Good
@@ -39,6 +47,11 @@ const reducer = (state = initialState, action) => {
 				...state,
 				numOfCakes: state.numOfCakes - 1,
 			};
+		case 'BUY_ICECREAM':
+			return {
+				...state,
+				numOfIceCream: state.numOfIceCream - 1,
+			};
 		default:
 			return state;
 	}
@@ -48,23 +61,25 @@ const reducer = (state = initialState, action) => {
 const redux = require('redux');
 const createStore = redux.legacy_createStore;
 
-// store holds application state
+//? store holds application state
 const store = createStore(reducer);
-// allows access to state via getState()
+//? allows access to state via getState()
 console.log('Initial State', store.getState());
-// registers listeners via subscribe(listener)
-store.subscribe(() => console.log('Updated state', store.getState()));
+//? registers listeners via subscribe(listener)
+// store.subscribe(() => console.log('Updated state', store.getState()));
 // allows state to be updated via dispatch(action)
-store.dispatch(buyCake());
-store.dispatch(buyCake());
-store.dispatch(buyCake());
-// handles un-registering of listeners via the function returned by subscriber(listener)
-// const unsubscribe = store.subscribe(() =>
-// 	console.log('Updated state', store.getState())
-// );
 // store.dispatch(buyCake());
 // store.dispatch(buyCake());
 // store.dispatch(buyCake());
-// unsubscribe();
+//? handles un-registering of listeners via the function returned by subscriber(listener)
+const unsubscribe = store.subscribe(() =>
+	console.log('Updated state', store.getState())
+);
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyIcecream());
+store.dispatch(buyCake());
+store.dispatch(buyIcecream());
+unsubscribe();
 
 //? Redux pattern :- you create a store, declare the initial state and reducer, define your action and actions creators, subscribe to the store, dispatch action to update the store and finally un-subscribe to the changes.
